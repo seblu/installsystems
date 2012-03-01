d5dd5a61b86ad1da92fd39e10edc1bbf1223461b
Sebastien Luttringer <sebastien.luttringer@smartjog.com>
2012-03-01 15:12:24 +0100

    allow ionice level to be setted

--- installsystems/config.py
+++ installsystems/config.py
@@ -62,7 +62,8 @@ class MainConfigFile(ConfigFile):
         "repo_filter": str,
         "repo_config": str,
         "nice": int,
-        "ionice": ["none", "rt", "be", "idle"]
+        "ionice_class": ["none", "rt", "be", "idle"],
+        "ionice_level": int
         }
     def __init__(self, filename, prefix=os.path.basename(sys.argv[0])):
         self.prefix = prefix
